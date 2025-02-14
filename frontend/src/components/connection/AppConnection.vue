c1723cff938cd61c8e54264dc12063ccc6990105
Mehdi <mehdi.mhiri@gmail.com>
2025-02-14 12:15:25 +0900

    fix(connection): handle long connection string

--- frontend/src/components/connection/AppConnection.vue
+++ frontend/src/components/connection/AppConnection.vue
@@ -43,65 +43,66 @@ function copyToClipboard(text: string) {
     @click="select(connection.id)"
   >
     <div class="flex gap-4 items-center">
-      <UIcon :name="`simple-icons:${connection.type}`" class="size-8" />
-      <div class="w-full flex flex-col">
-        <div class="flex flex-1 flex-row gap-2 items-center justify-between">
-          <span>
+      <div class="flex flex-initial">
+        <UIcon :name="`simple-icons:${connection.type}`" class="size-8" />
+      </div>
+      <div class="flex flex-1 flex-row gap-2 justify-between">
+        <UTooltip :text="connection.name" :content="{ side: 'right' }">
+          <span class="line-clamp-1 text-ellipsis">
             {{ getConnectionName(connection) }}
           </span>
-          <UPopover mode="hover" :content="{ side: 'right' }">
+        </UTooltip>
+        <UPopover mode="hover" :content="{ side: 'right' }">
+          <div class="flex">
             <UIcon
               name="lucide:info"
               class="size-6 text-secondary-400/50 hover:text-secondary-400 transition-colors"
             />
-            <template #content>
-              <div class="p-2 flex flex-col gap-2 text-gray-400">
-                <UTooltip text="Connection string" :content="{ side: 'left' }">
-                  <div class="flex flex-row gap-2 items-center">
-                    <UIcon name="lucide:link" class="text-secondary-400" />
-                    <UButton
-                      color="neutral"
-                      variant="ghost"
-                      trailing-icon="lucide:copy"
-                      :ui="{ base: 'px-1' }"
-                      :label="connection.connection_string"
-                      @click="copyToClipboard(connection.connection_string)"
-                    />
-                  </div>
-                </UTooltip>
-                <UTooltip text="Creation date" :content="{ side: 'left' }">
-                  <div class="flex flex-row gap-2 items-center">
-                    <UIcon
-                      name="lucide:calendar"
-                      :class="
-                        connection.created_at !== connection.updated_at
-                          ? 'text-primary-400/50'
-                          : 'text-primary-400'
-                      "
-                    />
-                    <span class="text-sm">
-                      {{ new Date(connection.created_at).toLocaleString() }}
-                    </span>
-                  </div>
-                </UTooltip>
-                <UTooltip text="Last update" :content="{ side: 'left' }">
-                  <div
-                    v-if="connection.created_at !== connection.updated_at"
-                    class="flex flex-row gap-2 items-center"
-                  >
-                    <UIcon
-                      name="lucide:calendar-sync"
-                      class="text-primary-400"
-                    />
-                    <span class="text-sm">
-                      {{ new Date(connection.updated_at).toLocaleString() }}
-                    </span>
-                  </div>
-                </UTooltip>
-              </div>
-            </template>
-          </UPopover>
-        </div>
+          </div>
+          <template #content>
+            <div class="p-2 flex flex-col gap-2 text-gray-400">
+              <UTooltip text="Connection string" :content="{ side: 'left' }">
+                <div class="flex flex-row gap-2 items-center">
+                  <UIcon name="lucide:link" class="text-secondary-400" />
+                  <UButton
+                    color="neutral"
+                    variant="ghost"
+                    trailing-icon="lucide:copy"
+                    :ui="{ base: 'px-1' }"
+                    :label="connection.connection_string"
+                    @click="copyToClipboard(connection.connection_string)"
+                  />
+                </div>
+              </UTooltip>
+              <UTooltip text="Creation date" :content="{ side: 'left' }">
+                <div class="flex flex-row gap-2 items-center">
+                  <UIcon
+                    name="lucide:calendar"
+                    :class="
+                      connection.created_at !== connection.updated_at
+                        ? 'text-primary-400/50'
+                        : 'text-primary-400'
+                    "
+                  />
+                  <span class="text-sm">
+                    {{ new Date(connection.created_at).toLocaleString() }}
+                  </span>
+                </div>
+              </UTooltip>
+              <UTooltip text="Last update" :content="{ side: 'left' }">
+                <div
+                  v-if="connection.created_at !== connection.updated_at"
+                  class="flex flex-row gap-2 items-center"
+                >
+                  <UIcon name="lucide:calendar-sync" class="text-primary-400" />
+                  <span class="text-sm">
+                    {{ new Date(connection.updated_at).toLocaleString() }}
+                  </span>
+                </div>
+              </UTooltip>
+            </div>
+          </template>
+        </UPopover>
       </div>
     </div>
 
