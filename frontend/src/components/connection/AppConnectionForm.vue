23ea8cf93ee5e6155a9118342191ec9c9d3c7e76
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2025-02-18 16:16:13 +0100

    Update AppConnectionForm.vue

--- frontend/src/components/connection/AppConnectionForm.vue
+++ frontend/src/components/connection/AppConnectionForm.vue
@@ -24,7 +24,11 @@ const schema = v.object({
   created_at: v.string(),
   updated_at: v.string(),
   name: v.string(),
-  type: v.string(),
+  type: v.union([
+    v.literal("sqlite"),
+    v.literal("mysql"),
+    v.literal("postgresql"),
+  ]),
   connection_string: v.string(),
 });
 const parser = v.safeParser(schema);
@@ -80,7 +84,7 @@ function selectFile() {
   );
 }
 
-function selectType(type: string) {
+function selectType(type: "sqlite" | "mysql" | "postgresql") {
   state.type = type;
   active.value = 1;
 }
