6b9acf18698a56a0087abbb04ae5d449bdf7d267
Mehdi <mehdi.mhiri@gmail.com>
2025-02-27 18:25:13 +0900

    chore(lint): commit prehook

---
+++ frontend/.lintstagedrc.js
@@ -0,0 +1,14 @@
+import path from "path";
+import process from "process";
+
+/**
+ * @param {string[]} filenames
+ */
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+export default {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+};
