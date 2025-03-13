0c08e41341510c1c3d960936e20ae13cacb2e4a6
Mehdi <mehdi.mhiri@gmail.com>
2025-03-14 00:42:08 +0900

    fix(connection): reset on welcome

--- frontend/src/components/AppWelcome.vue
+++ frontend/src/components/AppWelcome.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import { useApp } from "@/composables/shared/useApp";
+
+const { database, schema, table } = useApp();
+database.value = ""; // FIXME: reset schema var bc breadcrumb does not provide onclick
+schema.value = ""; // FIXME: reset schema var bc breadcrumb does not provide onclick
+table.value = ""; // FIXME: reset table var bc breadcrumb does not provide onclick
+</script>
+
 <template>
   <div class="flex flex-auto justify-center items-center">
     <UIcon name="noto:kiss-mark" class="size-96 select-none text-primary-400" />
