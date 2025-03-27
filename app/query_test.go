07feb4bae40d682c9242c23b0708312b15490580
Mehdi <mehdi.mhiri@gmail.com>
2025-03-28 00:14:13 +0900

    chore(go): add test for getconnectiondbs

--- app/query_test.go
+++ app/query_test.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"dbisous/app/client"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,5 +13,16 @@ func TestGetConnectionDatabases(t *testing.T) {
 	r.Equal(err, nil, err)
 	defer db.Close()
 
-	// TODO:
+	_ = createConnection(db, Connection{Type: PostgreSQL, Name: testConnectionName, ConnectionString: testPostgresConnectionString})
+	connections, err := getConnections(db)
+	connection := connections[0]
+	connect(activeConnections, db, connection.ID)
+
+	filters := make([]client.QueryFilter, 0)
+	filters = append(filters, client.QueryFilter{Column: "datname", Value: "'dbisous_test'"})
+	result, err := getConnectionDatabases(connection.ID, client.QueryParams{Offset: 0, Limit: 10, Filter: filters})
+	r.Equal(nil, err, err)
+	r.Equal(1, len(result.Rows), "Couldn't get databases")
+
+	// TODO: add separate tests for each param (offset, limit, filters, order)
 }
