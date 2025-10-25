2f839827c450f7a03fbdd18393315b90fbcb8af2
meeehdi-dev <mehdi.mhiri@gmail.com>
2025-10-25 21:06:21 +0900

    feat: page layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const items: NavigationMenuItem[] = [
+  {
+    label: "Home",
+    icon: "lucide:house",
+    active: true,
+  },
+];
+</script>
 
 <template>
   <UApp>
+    <UHeader title="DBisous" />
+
     <UMain>
-      <RouterView />
+      <UDashboardGroup class="top-(--ui-header-height)">
+        <UDashboardSidebar>
+          <UDashboardSearchButton />
+          <UNavigationMenu :items="items" orientation="vertical" />
+        </UDashboardSidebar>
+
+        <UDashboardSearch />
+
+        <RouterView />
+      </UDashboardGroup>
     </UMain>
   </UApp>
 </template>
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,24 +1,11 @@
-<script setup vapor lang="ts">
-import type { NavigationMenuItem } from "@nuxt/ui";
-
-const items: NavigationMenuItem[] = [
-  {
-    label: "Home",
-    icon: "lucide:house",
-    active: true,
-  },
-];
-</script>
+<script setup vapor lang="ts"></script>
 
 <template>
-  <UDashboardGroup>
-    <UDashboardSidebar collapsible>
-      <UNavigationMenu :items="items" orientation="vertical" />
-    </UDashboardSidebar>
-    <UDashboardPanel>
-      <template #header>
-        <UDashboardNavbar title="Home" icon="lucide:house" />
-      </template>
-    </UDashboardPanel>
-  </UDashboardGroup>
+  <UDashboardPanel id="home">
+    <template #header>
+      <UDashboardNavbar title="Home" icon="lucide:house" />
+    </template>
+
+    <template #body> coucou </template>
+  </UDashboardPanel>
 </template>
